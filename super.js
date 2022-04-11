8401449a1843e354872f2a72bf34422749d3f7ad
lukeskywalker007 <76431909+lukeskywalker007@users.noreply.github.com>
2022-04-11 15:29:23 +0530

    Add files via upload

---
+++ super.js
@@ -0,0 +1,19 @@
+var canvas=new fabric.Canvas('canvas');
+player_width=30;
+player_heigth=30;
+player_x=10;
+player_y=10;
+function update()
+{
+    
+}
+function up()
+{
+    if(player_y >=0)
+    {
+        console.log("block image height =" +block_image_height);
+        console.log("when up arrow key is pressed X ="+player_x+", Y ="+player_y);
+        canvas.remove(player_object);
+        player_update;
+    }
+}
